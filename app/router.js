4faf78cabb273b5ef2f9af7c808239319b42597f
Bennett Kolasinski <bennett.kolasinski@livenation.com>
2014-10-17 12:59:32 -0700

    basics

--- app/router.js
+++ app/router.js
@@ -6,6 +6,8 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('artists', function() { });
+  this.resource('venues', function() { });
 });
 
 export default Router;
